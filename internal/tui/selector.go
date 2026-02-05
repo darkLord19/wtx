3e71aa054eb9e9912bf64d30c71d8f2fa4418ba7
Umang Parmar <umangjparmar@gmail.com>
2026-02-05 16:27:17 +0530

    fix: build issues

--- internal/tui/selector.go
+++ internal/tui/selector.go
@@ -72,7 +72,7 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Message) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.list.SetWidth(msg.Width)
